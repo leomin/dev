2c8e3cb24e50924e253bdc4c402fbd83e45897a4
Robin Goins <pocketlark@gmail.com>
2014-10-19 00:20:06 -0500

    tumblr motion control script “fist” to click roll arm to scroll up and down

---
+++ Myo Armband/Tumblr_MotionControl_Myo.lua
@@ -0,0 +1,100 @@
+scriptId = 'com.thalmic.scripts.tumblr'
+
+-- Helpers
+
+-- Makes use of myo.getArm() to swap wave out and wave in when the armband is being worn on
+-- the left arm. This allows us to treat wave out as wave right and wave in as wave
+-- left for consistent direction. The function has no effect on other poses.
+	
+	
+function conditionallySwapWave(pose)
+    if myo.getArm() == "left" then
+        if pose == "waveIn" then
+            pose = "waveOut"
+        elseif pose == "waveOut" then
+            pose = "waveIn"
+        end
+    end
+    return pose
+end
+	
+function onPoseEdge(pose, edge)
+    myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+
+	if edge == "on" then
+		--grab/select things in game with Fist pose	
+		if pose == "fist" then
+			myo.mouse("left","down")
+			restvar = math.abs(math.deg(myo.getRoll()));
+		end
+		
+		--right click mouse using FingersSpread pose
+		if pose == "fingersSpread" then
+			myo.mouse("right","down")
+		end
+		
+		--scroll with roll
+		local direction = myo.getXDirection()
+		myo.debug(math.deg(myo.getRoll()))
+		myo.debug(myo.getXDirection())
+		--if
+		--	myo.debug(math.deg(myo.getRoll()))
+		--	myo.keyboard("down_arrow", "down")
+		--end
+			
+	end
+	if edge == "off" then
+		myo.mouse("left","up")
+		myo.mouse("right","up")
+		myo.keyboard("up_arrow","up")
+		myo.keyboard("down_arrow","up")
+	end
+end
+
+function pageScroll()
+	
+end
+
+-- Unlock mechanism
+
+function unlock()
+    unlocked = true
+    extendUnlock()
+end
+
+-- unnecessary for this app
+function extendUnlock()
+    unlockedSince = myo.getTimeMilliseconds()
+end
+
+function onPeriodic()
+		if math.abs(math.deg(myo.getRoll())) > (9 + restvar) then
+			myo.keyboard("down_arrow","down")
+		elseif math.abs(math.deg(myo.getRoll())) < (restvar-9) then
+			myo.keyboard("up_arrow","down")
+		end
+end
+
+function onForegroundWindowChange(app, title)
+    -- Here we decide if we want to control the new active app.
+    --myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    if title == string.match(title, '.*Tumblr*.') then		
+		myo.controlMouse(true)
+		myo.debug("mouse enabled")
+        return true
+		
+    end
+    return false
+end
+
+function activeAppName()
+    -- Return the active app name determined in onForegroundWindowChange
+    return "Tumblr"
+end
+
+function onActiveChange(isActive)
+    myo.debug("onActiveChange")
+    if not isActive then
+        unlocked = false
+    end
+end
